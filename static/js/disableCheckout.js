5fd3cb5e79d6b80a68e0846d6922fd5715d025de
Camilo Cespedes <cacespedes@uninorte.edu.co>
2023-05-28 07:15:45 -0500

    Removed debug logs

--- static/js/disableCheckout.js
+++ static/js/disableCheckout.js
@@ -1,9 +1,7 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", () => {
     var cart = Cart.get();
-    console.log("Llego aca")
     if (cart.isEmpty())
-        console.log("Cart: "+cart+". Is empty")
         document.getElementById("checkout-btn").style.pointerEvents = "none";
         document.getElementById("checkout-btn").style.opacity = "0.5";
 });
